8c19f479b5da04642ee7266b25d21279046aa3dd
Konstantin Ershov <konstantin.ershov@gmail.com>
2020-01-19 13:23:30 +0300

    Optimized imports

--- src/main/java/ru/kershov/blogapp/BlogAppApplication.java
+++ src/main/java/ru/kershov/blogapp/BlogAppApplication.java
@@ -4,9 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.kershov.blogapp.model.User;
-import ru.kershov.blogapp.model.dao.SettingsDAO;
 import ru.kershov.blogapp.model.SettingsInit;
+import ru.kershov.blogapp.model.dao.SettingsDAO;
 
 @SpringBootApplication
 public class BlogAppApplication implements CommandLineRunner {
