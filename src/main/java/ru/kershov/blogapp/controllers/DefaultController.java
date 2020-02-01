18696f2ef3e438ef43bb3b66d5979ba068f9dbea
Konstantin Ershov <konstantin.ershov@gmail.com>
2020-02-01 17:04:04 +0300

    Fixed entrypoint: redirect > forward for proper handling requests by Vue.js app

--- src/main/java/ru/kershov/blogapp/controllers/DefaultController.java
+++ src/main/java/ru/kershov/blogapp/controllers/DefaultController.java
@@ -1,7 +1,6 @@
 package ru.kershov.blogapp.controllers;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +9,6 @@ public class DefaultController {
     // Will match all paths that don't contain a period and are not already mapped to another controller
     @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, value = "/**/{path:[^\\.]*}")
     public String redirectToIndex() {
-        return "redirect:/";
+        return "forward:/";
     }
 }
